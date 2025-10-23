b1feced50987ec53fc79622f9261a5812d53364a
mvarzideh <m.varzideh@gmail.com>
2025-10-23 15:32:17 +0330

    Second commit

---
+++ expense.py
@@ -0,0 +1,16 @@
+expenses=[10,13.4,14.56,19.45,13.12]
+total=sum(expenses)
+print("you spent $",total)
+
+expenses=[]
+expenses.append(float(input("enter an expense:\n")))
+expenses.append(float(input("enter an expense:\n")))
+expenses.append(float(input("enter an expense:\n")))
+expenses.append(float(input("enter an expense:\n")))
+expenses.append(float(input("enter an expense:\n")))
+expenses.append(float(input("enter an expense:\n")))
+expenses.append(float(input("enter an expense:\n")))
+
+total=sum(expenses)
+print("you spent $",total)
+
