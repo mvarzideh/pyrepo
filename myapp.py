c23e09c758d4ced15762558aa3c5f6e9a36e537e
mvarzideh <m.varzideh@gmail.com>
2025-10-23 15:32:43 +0330

    Third commit

---
+++ myapp.py
@@ -0,0 +1,5 @@
+age=int(input("let me know your age:\n "))
+print(age)
+decades=age // 10
+years=age % 10
+print("your age is " , decades , "decades and"  , years , "years")
---
+++ sample.py
@@ -0,0 +1,4 @@
+mynum=int(input("please enter a number:\n"))
+print("entered number is" , mynum)
+print("entered number is " + str(mynum))
+print(f"entered number is {mynum}" )
